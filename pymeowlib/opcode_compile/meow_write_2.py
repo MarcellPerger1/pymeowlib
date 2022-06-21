22170f0cdba304b04522ce9360cd3561a2758ac5
MarcellPerger1 <102254594+MarcellPerger1@users.noreply.github.com>
2022-06-21 07:45:29 +0000

    Remove more old code

--- pymeowlib/opcode_compile/meow_write_2.py
+++ pymeowlib/opcode_compile/meow_write_2.py
@@ -1,40 +1,4 @@
-import dis
-from abc import ABC, abstractmethod
 from .opcode_util import OPS, for_op
-
-
-
-
-class PyWriter:
-    pass
-
-
-class OpcodeWriter(ABC):
-    def __init__(self, writer):
-        self.writer = writer
-    
-    @abstractmethod
-    def write(self):
-        pass
-
-
-
-
-
-
-class PyParser:
-    def __init__(self, prog):
-        self.prog = prog
-        self.instr_list = list(dis.get_instructions(self.prog))
-
-    def parse(self):
-        for instr in self.instr_list:
-            self.instr: dis.Instruction = instr
-            opc = self.instr.opcode
-            if opc == OPS.NOP:
-                pass
-            elif opc == OPS.POP_TOP:
-                pass
 
 
 def addBlock(target, block):
