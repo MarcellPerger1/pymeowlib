6448c2a7fd209d457060b5100ec5c4cc93fae17c
MarcellPerger1 <102254594+MarcellPerger1@users.noreply.github.com>
2022-06-21 07:44:10 +0000

    Move some code (separate files is good)

--- pymeowlib/opcode_compile/meow_write_2.py
+++ pymeowlib/opcode_compile/meow_write_2.py
@@ -1,7 +1,6 @@
 import dis
-import re
 from abc import ABC, abstractmethod
-from .opcode_util import OPS
+from .opcode_util import OPS, for_op
 
 
 
@@ -19,43 +18,7 @@ class OpcodeWriter(ABC):
         pass
 
 
-OP_TO_FUNC = {}
 
-def for_op(o):
-    name = o
-    def set_as_op(f):
-        OP_TO_FUNC[_to_opcode(name)] = f
-        return f
-    if callable(o):
-        name = o.__name__
-        return set_as_op(o)
-    return set_as_op
-
-def _to_opcode(o):
-    if isinstance(o, int):
-        return o
-    else:
-        return to_snake_case(o, True)
-
-# upper: bool or None(=preserve) 
-def to_snake_case(name: str, upper=False):
-    res = ''
-    i = 0
-    while i < len(name):
-        if i == 0:
-            res += name[i]
-            i += 1
-            continue
-        if name[i] in ' -_':
-            res += '_'
-        if name[i-1].islower() and name[i].isupper():
-            res += '_' + name[i]
-        else:
-            res += name[i]
-        i += 1
-    if upper is None:
-        return res
-    return res.upper() if upper else res.lower()
 
 
 
