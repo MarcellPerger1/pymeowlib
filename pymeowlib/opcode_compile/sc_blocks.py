7cad7d43b3385aca8a612d8c5c2a55fae67489f0
MarcellPerger1 <102254594+MarcellPerger1@users.noreply.github.com>
2022-07-06 12:16:57 +0000

    feat: finish generic (forgot to git add)

--- pymeowlib/opcode_compile/sc_blocks.py
+++ pymeowlib/opcode_compile/sc_blocks.py
@@ -63,11 +63,26 @@ def _listitem_proxy(target, item):
     return ContainerProxy(lambda: target[item],
                           lambda v: op.setitem(target, item, v))
 
-def generic(target):
+def generic(clsarg=None, target=None):
     def decor(cls):
         assert issubclass(cls, target) and cls is not target
-        def init(self, ): # ...
-            ...
+        def init(self, name, *args, **kwargs):
+            self.name = name
+            target.__init__(self, *args, **kwargs)
+        init.__name__ = "__init__"
+        init.__qualname__ = cls.__qualname__ + "." + init.__name__
+        cls.__init__ = init
+    if clsarg is None:
+        if target is not None:
+            return decor
+        return generic
+    if target is None:
+        for b in clsarg.__bases__:
+            if issubclass(b, ScratchBlock):
+                target = b
+                return decor(clsarg)
+    return decor(clsarg)
+    
 
 
 class Named:
