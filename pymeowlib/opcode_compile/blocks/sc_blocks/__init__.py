cca2dfb10d9f31b2f54d34cd0873f0e306d012e5
MarcellPerger1 <102254594+MarcellPerger1@users.noreply.github.com>
2022-07-08 12:12:41 +0000

    directory structure :)

---
+++ pymeowlib/opcode_compile/blocks/sc_blocks/__init__.py
@@ -0,0 +1,2 @@
+from .sc_blocks import *
+from .c_blocks import *
---
+++ pymeowlib/opcode_compile/blocks/sc_blocks/c_blocks.py
@@ -0,0 +1,45 @@
+class ForeverBlock(BaseCBlock):
+    name = "doForever"
+
+    def __init__(self, blocks=()):
+        super().__init__([])
+        self.add(*blocks)
+
+    def add(self, *blocks):
+        self.data[1].extend(blocks)
+
+
+class UntilBlock(BaseCondCBlock):
+    name = "doUntil"
+
+
+class RepeatBlock(BaseCondCBlock):
+    name = "doRepeat"
+
+
+# conditions
+class IfBlock(BaseCondCBlock):
+    name = "doIf"
+
+
+class IfElseBlock(IfBlock):
+    name = "doIfElse"
+
+    def __init__(self, cond, if_=(), else_=()):
+        super(BaseCondCBlock, self).__init__(cond, [], [])
+        self.if_ = ListitemProxy(self.data, 2)
+        self.else_ = ListitemProxy(self.data, 3)
+        self.add_if(if_)
+        self.add_else(else_)
+
+    def add(self, *blocks, target="if"):
+        if target == "if":
+            self.add_if(*blocks)
+        else:
+            self.add_else(*blocks)
+
+    def add_if(self, *blocks):
+        self.data[2].extend(*blocks)
+
+    def add_else(self, *blocks):
+        self.data[3].extend(blocks)
