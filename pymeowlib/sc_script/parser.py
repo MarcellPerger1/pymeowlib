5d883d8af001b51e35edac0a34bca0386fa54668
MarcellPerger1 <marcell.perger.1@gmail.com>
2022-07-11 19:34:49 +0100

    Finish assignment LHS handling

--- pymeowlib/sc_script/parser.py
+++ pymeowlib/sc_script/parser.py
@@ -3,6 +3,8 @@ from __future__ import annotations
 import re
 from typing import Optional
 
+from ..opcode_compile.blocks import Block
+
 STR_RE = re.compile(r'"((?:[^"\\]|\\.)*?)"')
 IDENT_RE = re.compile(r'([a-zA-Z_]\w*)')
 LIST_IDENT_RE = re.compile(r'[a-zA-Z_$][\w$]*')
@@ -49,20 +51,44 @@ class Parser:
             self._handle_line()
 
     def _handle_line(self):
-        line = self.line.strip()
-        self.left, self.right = map(str.strip, line.split('='))
+        self.smt: Optional[Block] = None
+        self.line = self.line.strip()
+        self.left, self.right = map(str.strip, self.line.split('='))
         self._handle_left()
         ...
 
     def _handle_left(self):
+        if self._check_var_assign():
+            self._init_var_assign()
+            return
+        if self._check_listitem_assign():
+            self._init_listitem_assign()
+            return
+        raise SyntaxError("Invalid LHS of assignment")
+
+    def _check_var_assign(self):
         m = IDENT_RE.fullmatch(self.left)
         if m:
             self.target = 'var'
             self.ident = m[1]
-            return
+            return True
+
+    def _init_var_assign(self):
+        self.smt = Block("setVar:to:", self.ident, None)
+
+    def _check_listitem_assign(self):
         m = LISTITEM_RE.fullmatch(self.left)
         if m:
             self.target = 'list'
             self.ident, self.index = m[1], m[2]
-            return
-        raise SyntaxError("Invalid value on LHS of assignment")
+            return True
+
+    def _init_listitem_assign(self):
+        self.smt = Block("setLine:ofList:to:", int(self.index), self.ident, None)
+
+    def _set_assign_target(self, target):
+        if self.target == 'var':
+            self.smt.data[2] = target
+        elif self.target == 'list':
+            self.smt.data[3] = target
+        assert False
