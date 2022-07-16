2410d6ef8036d846936a39fdac5f00ce5a8777b1
MarcellPerger1 <marcell.perger.1@gmail.com>
2022-07-16 18:15:34 +0100

    Add Tokenizer class

---
+++ pymeowlib/sc_script/tokenizer.py
@@ -0,0 +1,10 @@
+class Tokenizer:
+    def __init__(self, src: str):
+        self.src = src
+
+    def tokenize(self):
+        self._tokenize()
+        return self
+
+    def _tokenize(self):
+        pass
