685ea646aaae5f277886e2f7a74ff1adf28f9d73
MarcellPerger1 <102254594+MarcellPerger1@users.noreply.github.com>
2023-03-16 08:28:36 +0000

    refactor: Don't use `ListitemProxy` as it made the code very confusing

--- pymeowlib/opcode_compile/blocks/sc_blocks/c_blocks.py
+++ pymeowlib/opcode_compile/blocks/sc_blocks/c_blocks.py
@@ -31,10 +31,22 @@ class IfElseBlock(IfBlock):
 
     def __init__(self, cond, if_=(), else_=()):
         super(BaseCondCBlock, self).__init__(cond, [], [])
-        self.if_ = ListitemProxy(self.data, 2)
-        self.else_ = ListitemProxy(self.data, 3)
         self.add_if(if_)
         self.add_else(else_)
+
+    @property
+    def if_(self):
+        return self.data[2]
+    @if_.setter
+    def if_(self, value):
+        self.data[2] = value
+
+    @property
+    def else_(self):
+        return self.data[3]
+    @else_.setter
+    def else_(self, value):
+        self.data[3] = value
 
     def add(self, *blocks, target="if"):
         if target == "if":
@@ -43,7 +55,7 @@ class IfElseBlock(IfBlock):
             self.add_else(*blocks)
 
     def add_if(self, *blocks):
-        self.data[2].extend(*blocks)
+        self.if_.extend(*blocks)
 
     def add_else(self, *blocks):
-        self.data[3].extend(blocks)
+        self.else_.extend(blocks)
