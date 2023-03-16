141d7478424658964e786bc50d66240def1b1beb
MarcellPerger1 <102254594+MarcellPerger1@users.noreply.github.com>
2023-03-16 11:52:45 +0000

    refactor: Rewrite `@generic` decorator and add docs for it

--- pymeowlib/opcode_compile/blocks/util/generic_block.py
+++ pymeowlib/opcode_compile/blocks/util/generic_block.py
@@ -1,7 +1,19 @@
 from ..bases import ScratchBlock
 
 
+def get_base_block_class(cls):
+    for b in cls.__bases__:
+        if issubclass(b, ScratchBlock):
+            return b
+
+
 def generic(clsarg=None, target=None):
+    """Sets the __init__ to a generic version with the signature 
+    __init__(name, *args, **kwargs) with args and kwargs being the
+    arguments of passed to the target __init__. target is the 
+    'base' sprite class, the new __init__ will call the __init__
+    of this class after setting the name. If not set, target is
+    the first base that is a ScratchBlock"""
     def decor(cls):
         assert issubclass(cls, target) and cls is not target
 
@@ -13,13 +25,10 @@ def generic(clsarg=None, target=None):
         init.__qualname__ = cls.__qualname__ + "." + init.__name__
         cls.__init__ = init
         return cls
-    if clsarg is None:
-        if target is not None:
-            return decor
-        return generic
-    if target is None:
-        for b in clsarg.__bases__:
-            if issubclass(b, ScratchBlock):
-                target = b
-                return decor(clsarg)
-    return decor(clsarg)
+    if clsarg is not None:
+        if target is None:
+            target = get_base_block_class(clsarg)
+        return decor(clsarg)
+    if target is not None:
+        return decor
+    return generic
